b7dea41694ad63a52e12a2cb609041d1f043c898
makoto-ito <makoto65053117@gmail.com>
2022-10-21 20:16:58 +0900

    [fix] プレイヤーの動きを修正

--- Assets/PMove.cs
+++ Assets/PMove.cs
@@ -27,13 +27,11 @@ public class PMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Pmove();
-        //Pmove2();
-        Pmove3();
+        Pmove();
 
     }
 
-    //プレイヤーを動かすための関数（現在うまく動かない）
+    /// <summary>プレイヤーを動かすための関数 </summary>
     void Pmove()
     {
         var velox = Input.GetAxisRaw("Horizontal");
@@ -70,6 +68,7 @@ public class PMove : MonoBehaviour
                         areaController._onPlayer = false;
                         this.transform.position = new Vector3(MapManager._areas[_pointX + 1, _pointZ].transform.position.x, this.transform.position.y, this.transform.position.z);
                         _pointX = _pointX + 1;
+                        _buttonDown = true;
                     }
 
                     //this.transform.position = new Vector3(MapManager._areas[_pointX + 1, _pointZ].transform.position.x, MapManager._areas[_pointX + 1, _pointZ].transform.position.y, MapManager._areas[_pointX + 1, _pointZ].transform.position.z);
@@ -94,14 +93,16 @@ public class PMove : MonoBehaviour
                         areaController._onPlayer = false;
                         this.transform.position = new Vector3(MapManager._areas[_pointX - 1, _pointZ].transform.position.x, this.transform.position.y, this.transform.position.z);
                         _pointX = _pointX - 1;
+                        _buttonDown = true;
                     }
                 }
             }
             else
             {
                 Debug.Log("MISS");
+                _buttonDown = true;
             }
-            _buttonDown = true;
+            
         }
 
         if (Input.GetButtonDown("Vertical"))
@@ -129,6 +130,7 @@ public class PMove : MonoBehaviour
                         areaController._onPlayer = false;
                         this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, MapManager._areas[_pointX, _pointZ + 1].transform.position.z);
                         _pointZ = _pointZ + 1;
+                        _buttonDown = true;
                     }
 
                 }
@@ -153,6 +155,7 @@ public class PMove : MonoBehaviour
                         areaController._onPlayer = false;
                         this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, MapManager._areas[_pointX, _pointZ - 1].transform.position.z);
                         _pointZ = _pointZ - 1;
+                        _buttonDown = true;
                     }
 
                 }
@@ -160,259 +163,10 @@ public class PMove : MonoBehaviour
             else
             {
                 Debug.Log("MISS");
-            }
-        }
-        _buttonDown = true;
-    }
-
-    //うまく動かなかったため原因を探すための関数（Inputが多いのではと感じ減らしてみた関数）
-    void Pmove2()
-    {
-        var velox = Input.GetAxisRaw("Horizontal");
-        var vertical = Input.GetAxisRaw("Vertical");
-        //if(_buttonDown == false)//入力キーを押されたときに敵も一緒に動いてほしいので
-        //{
-        //    RizumuController._eMoveFlag = true;
-        //}
-
-        if (RizumuController._moveFlag == true && _buttonDown == false)
-        {
-            if (velox > 0)
-            {
-                //行きたい方向の情報を確認したいので移動先のスクリプトを取得する
-                areaController = MapManager._areas[_pointX + 1, _pointZ].GetComponent<AreaController>();
-
-                //移動先の情報によって行動を決める
-                if (areaController._onWall == true)
-                {
-
-                }
-                else if (areaController._onEnemy == true)
-                {
-
-                }
-                else
-                {
-                    areaController._onPlayer = true;
-                    areaController = MapManager._areas[_pointX, _pointZ].GetComponent<AreaController>();
-                    areaController._onPlayer = false;
-                    this.transform.position = new Vector3(MapManager._areas[_pointX + 1, _pointZ].transform.position.x, this.transform.position.y, this.transform.position.z);
-                    _pointX = _pointX + 1;
-                }
-
                 _buttonDown = true;
-
-                //this.transform.position = new Vector3(MapManager._areas[_pointX + 1, _pointZ].transform.position.x, MapManager._areas[_pointX + 1, _pointZ].transform.position.y, MapManager._areas[_pointX + 1, _pointZ].transform.position.z);
-            }
-            else if (velox < 0)
-            {
-                areaController = MapManager._areas[_pointX - 1, _pointZ].GetComponent<AreaController>();
-
-                //移動先の情報によって行動を決める
-                if (areaController._onWall == true)
-                {
-
-                }
-                else if (areaController._onEnemy == true)
-                {
-
-                }
-                else
-                {
-                    areaController._onPlayer = true;
-                    areaController = MapManager._areas[_pointX, _pointZ].GetComponent<AreaController>();
-                    areaController._onPlayer = false;
-                    this.transform.position = new Vector3(MapManager._areas[_pointX - 1, _pointZ].transform.position.x, this.transform.position.y, this.transform.position.z);
-                    _pointX = _pointX - 1;
-                }
-
-                _buttonDown = true;
-
-            }
-            else if (vertical > 0)
-            {
-                //行きたい方向の情報を確認したいので移動先のスクリプトを取得する
-                areaController = MapManager._areas[_pointX, _pointZ + 1].GetComponent<AreaController>();
-
-                //移動先の情報によって行動を決める
-                if (areaController._onEnemy == true)
-                {
-
-                }
-                else if (areaController._onWall == true)
-                {
-
-                }
-                else
-                {
-                    areaController._onPlayer = true;
-                    areaController = MapManager._areas[_pointX, _pointZ].GetComponent<AreaController>();
-                    areaController._onPlayer = false;
-                    this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, MapManager._areas[_pointX, _pointZ + 1].transform.position.z);
-                    _pointZ = _pointZ + 1;
-                }
-
-                _buttonDown = true;
-
-            }
-            else if (vertical < 0)
-            {
-                //行きたい方向の情報を確認したいので移動先のスクリプトを取得する
-                areaController = MapManager._areas[_pointX, _pointZ - 1].GetComponent<AreaController>();
-
-                //移動先の情報によって行動を決める
-                if (areaController._onEnemy == true)
-                {
-
-                }
-                else if (areaController._onWall == true)
-                {
-
-                }
-                else
-                {
-                    areaController._onPlayer = true;
-                    areaController = MapManager._areas[_pointX, _pointZ].GetComponent<AreaController>();
-                    areaController._onPlayer = false;
-                    this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, MapManager._areas[_pointX, _pointZ - 1].transform.position.z);
-                    _pointZ = _pointZ - 1;
-                }
-
-                _buttonDown = true;
-
-            }
-        }
-        else
-        {
-
-            if (velox > 0 || velox < 0 || vertical > 0 || vertical < 0)
-            {
-                _buttonDown = true;
-                Debug.Log("MISS");
             }
         }
     }
-
-    //上とはやり方で原因を探す関数（ちゃんと動きはしているのかの確認）
-    void Pmove3()
-    {
-        var velox = Input.GetAxisRaw("Horizontal");
-        var vertical = Input.GetAxisRaw("Vertical");
-
-
-        if (Input.GetButtonDown("Horizontal"))
-        {
-            //if(_buttonDown == false)//入力キーを押されたときに敵も一緒に動いてほしいので
-            //{
-            //    RizumuController._eMoveFlag = true;
-            //}
-            if (velox > 0)
-            {
-                //行きたい方向の情報を確認したいので移動先のスクリプトを取得する
-                areaController = MapManager._areas[_pointX + 1, _pointZ].GetComponent<AreaController>();
-
-                //移動先の情報によって行動を決める
-                if (areaController._onWall == true)
-                {
-
-                }
-                else if (areaController._onEnemy == true)
-                {
-
-                }
-                else
-                {
-                    areaController._onPlayer = true;
-                    areaController = MapManager._areas[_pointX, _pointZ].GetComponent<AreaController>();
-                    areaController._onPlayer = false;
-                    this.transform.position = new Vector3(MapManager._areas[_pointX + 1, _pointZ].transform.position.x, this.transform.position.y, this.transform.position.z);
-                    _pointX = _pointX + 1;
-                }
-
-                //this.transform.position = new Vector3(MapManager._areas[_pointX + 1, _pointZ].transform.position.x, MapManager._areas[_pointX + 1, _pointZ].transform.position.y, MapManager._areas[_pointX + 1, _pointZ].transform.position.z);
-            }
-            else if (velox < 0)
-            {
-                areaController = MapManager._areas[_pointX - 1, _pointZ].GetComponent<AreaController>();
-
-                //移動先の情報によって行動を決める
-                if (areaController._onWall == true)
-                {
-
-                }
-                else if (areaController._onEnemy == true)
-                {
-
-                }
-                else
-                {
-                    areaController._onPlayer = true;
-                    areaController = MapManager._areas[_pointX, _pointZ].GetComponent<AreaController>();
-                    areaController._onPlayer = false;
-                    this.transform.position = new Vector3(MapManager._areas[_pointX - 1, _pointZ].transform.position.x, this.transform.position.y, this.transform.position.z);
-                    _pointX = _pointX - 1;
-                }
-            }
-
-            _buttonDown = true;
-            Debug.Log("現在の配列番号"　+ _pointX + " , " + _pointZ);
-        }
-
-        if (Input.GetButtonDown("Vertical"))
-        {
-            if (vertical > 0)
-            {
-                //行きたい方向の情報を確認したいので移動先のスクリプトを取得する
-                areaController = MapManager._areas[_pointX, _pointZ + 1].GetComponent<AreaController>();
-
-                //移動先の情報によって行動を決める
-                if (areaController._onEnemy == true)
-                {
-
-                }
-                else if (areaController._onWall == true)
-                {
-
-                }
-                else
-                {
-                    areaController._onPlayer = true;
-                    areaController = MapManager._areas[_pointX, _pointZ].GetComponent<AreaController>();
-                    areaController._onPlayer = false;
-                    this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, MapManager._areas[_pointX, _pointZ + 1].transform.position.z);
-                    _pointZ = _pointZ + 1;
-                }
-
-            }
-            else if (vertical < 0)
-            {
-                //行きたい方向の情報を確認したいので移動先のスクリプトを取得する
-                areaController = MapManager._areas[_pointX, _pointZ - 1].GetComponent<AreaController>();
-
-                //移動先の情報によって行動を決める
-                if (areaController._onEnemy == true)
-                {
-
-                }
-                else if (areaController._onWall == true)
-                {
-
-                }
-                else
-                {
-                    areaController._onPlayer = true;
-                    areaController = MapManager._areas[_pointX, _pointZ].GetComponent<AreaController>();
-                    areaController._onPlayer = false;
-                    this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, MapManager._areas[_pointX, _pointZ - 1].transform.position.z);
-                    _pointZ = _pointZ - 1;
-                }
-
-            }
-        }
-        _buttonDown = true;
-    }
-
-
 
     private void OnCollisionEnter(Collision collision)
     {
