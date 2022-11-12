2fe59e544be3ba11b41befe7ed054b504b594a2b
makoto-ito <makoto65053117@gmail.com>
2022-11-12 12:40:04 +0900

    [add] 敵を管理するスクリプトフォルダを追加

---
+++ Assets/Script/Enemy/EnemyModel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyModel 
+{
+    /// <summary>“G‚ÌHP</summary>
+    [SerializeField] int _enemyHP = 2;
+
+    
+}
--- Assets/Script/Enemy/EnemyView.cs
+++ Assets/Script/Enemy/EnemyView.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyData : MonoBehaviour
+public class EnemyView : MonoBehaviour
 {
-    /// <summary>“G‚ÌHP</summary>
-    [SerializeField] int _enemyHP = 2;
-
     // Start is called before the first frame update
     void Start()
     {
